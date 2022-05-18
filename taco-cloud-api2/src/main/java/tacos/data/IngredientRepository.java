db58379e48ad3f96089ffcb0bae15454110b3277
Quynh <cherryvuk29@gmail.com>
2022-05-18 17:47:15 +0700

    g

---
+++ taco-cloud-api2/src/main/java/tacos/data/IngredientRepository.java
@@ -0,0 +1,7 @@
+package tacos.data;
+
+import tacos.Ingredient;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
+}
